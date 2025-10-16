1ea971df4cf81892a87f359925eea03f728b6bf2
Xharless <carlosarevalo1702@hotmail.com>
2025-10-16 14:39:21 -0300

    cambio en la jerarquia de carpetas

--- project/frontend/cypress.config.js
+++ project/frontend/cypress.config.js
@@ -2,14 +2,14 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: '../tests/e2e/**/*.cy.{js,jsx,ts,tsx}', 
+    specPattern: '../tests/Cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', 
     baseUrl: 'http://localhost:5173', // Ajusta según la URL de tu frontend
     setupNodeEvents(on, config) {
       return config;
     },
     reporter: 'mocha-junit-reporter',
     reporterOptions: {
-      mochaFile: '../tests/reports/cypress/results-[hash].xml',
+      mochaFile: '../tests/Cypress/reports/results-[hash].xml',
       toConsole: true,
       useFullTitle: true, 
     },
